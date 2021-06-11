4aab6c8965555b2c9222bb7f02df3604ca651cfd
Слава <slavadodonov19@gmail.com>
2021-06-11 14:26:43 +0300

    Features:
    1. Slots
    2. Aliases
    3. Locales
    4. Handing errors

--- scripts/index.js
+++ scripts/index.js
@@ -1,41 +1,46 @@
-const example = [
+const components = {};
+
+let aliasesList = {
+    slot: 'slot',
+    locale: 'locale',
+    prop: 'prop'
+};
+
+const avatar = JSON.stringify({
+    div: {
+        props: {
+            class: 'logoBlock'
+        },
+        a: {
+            props: {
+                href: "https://vk.com"
+            },
+            img: {
+                props: {
+                    src: 'prop:logoSrc'
+                }
+            },
+            div: 'slot:avatar',
+            p: ['locale:text', 'locale:title']
+        }
+    }
+})
+
+const example = JSON.stringify([
     {
         div: {
+            props: {
+                class: 'logoBlock'
+            },
             a: {
                 props: {
                     href: "https://vk.com"
                 },
-                p: {
-                    props: {
-                        class: 'text'
-                    },
-                    b: 'Hello, world!',
-                    span: {
-                        props: {
-                            class: 'subtitle'
-                        },
-                        div: {
-                            span: {
-                                p: ['Hello!', 'Hi!', 'Bonjour!']
-                            }
-                        },
-                        span: {
-                            props: {
-                                class: 'main'
-                            },
-                            content: [
-                                {
-                                    p: {
-                                        props: {
-                                            class: 'text'
-                                        },
-                                        content: ["Hi!", "Hello!"]
-                                    }
-                                },
-                                {
-                                    p: "Bonjour!"
-                                }
-                            ]
+                p: ['locale:text', 'locale:title'],
+                Avatar: {
+                    slots: {
+                        'slot:avatar': {
+                            div: 'Hello!'
                         }
                     }
                 }
@@ -43,9 +48,16 @@ const example = [
         }
     },
     {
-        div: null
+        div: 'locale:block'
     }
-]
+])
+
+const editAliasesList = (list) => {
+    aliasesList = { ...aliasesList, ...list };
+}
+
+const getFirstLetter = (str) => str.substr(0, 1);
+const isComponent = (name) => getFirstLetter(name) === getFirstLetter(name).toUpperCase();
 
 const parseParams = (params) => {
     if (typeof params === 'string') {
@@ -104,24 +116,66 @@ const addContent = (element, content) => {
     }
 }
 
-const addChildren = (element, children) => {
+const hasAlias = (alias) => {
+    const value = /^(.*?)\:/g.exec(alias);
+
+    if (value) return !!aliasesList[value[1]];
+}
+
+const addAliases = (object, replacer) => {
+    const clonedObject = JSON.parse(object);
+
+    const replaceValue = (object, replacer) => {
+        for (let prop in object) {
+            if (typeof object[prop] !== "string") {
+                replaceValue(object[prop], replacer);
+            } else {
+                if (hasAlias(object[prop])) {
+                    const replaceValue = replacer[object[prop]];
+                    if (replaceValue) object[prop] = replaceValue;
+                }
+            }
+        }
+    };
+
+    replaceValue(clonedObject, replacer);
+
+    return clonedObject;
+};
+
+const addComponent = (element, name, props) => {
+    const component = addAliases(components[name], props);
+
+    addContent(element, component);
+}
+
+const addChildren = (element, children, aliases) => {
     for (const tag in children) {
+        if (isComponent(tag) && components[tag]) {
+            const { props, slots } = children[tag] || {};
+
+            addComponent(element, tag, { ...aliases, ...props, ...slots }, slots);
+            continue;
+        }
+
         const { content, props } = parseParams(children[tag]);
 
         if (Array.isArray(content)) {
             if (content.every((elem) => typeof elem.content !== 'string')) {
                 const child = createElement(tag, { props, content });
 
-                return element.appendChild(child);
+                element.appendChild(child);
+            } else {
+                content.forEach((elem) => {
+                    if (typeof elem.content === 'string') {
+                        const child = createElement(tag, { props, content: elem.content });
+
+                        element.appendChild(child);
+                    }
+                })
             }
 
-            return content.forEach((elem) => {
-                if (typeof elem.content === 'string') {
-                    const child = createElement(tag, { props, content: elem.content });
-
-                    return element.appendChild(child);
-                }
-            })
+            continue;
         }
 
         const child = createElement(tag, { props, content })
@@ -130,16 +184,29 @@ const addChildren = (element, children) => {
     }
 }
 
-const render = (object) => {
-    if (Array.isArray(object)) {
-        return object.forEach((elem) => {
-            render(elem);
+const render = (object, aliases) => {
+    const parsed = addAliases(object, aliases);
+
+    if (Array.isArray(parsed)) {
+        return parsed.forEach((param) => {
+            for (const tag in param) {
+                //TODO: Рендеринг компонентов на верхнем уровне
+                console.log(createElement(tag, parseParams(param[tag]), aliases));
+            }
         })
     }
 
-    for (const tag in object) {
-        console.log(createElement(tag, parseParams(object[tag])));
+    for (const tag in parsed) {
+        //TODO: Рендеринг компонентов на верхнем уровне
+        console.log(createElement(tag, parseParams(parsed[tag]), aliases));
     }
 }
 
-render(example)
+const registerComponent = (name, content, aliases) => {
+    if (!isComponent(name)) throw new Error('The component name must start with a capital letter');
+
+    components[name] = JSON.stringify(addAliases(content, aliases));
+}
+
+registerComponent('Avatar', avatar, { 'locale:text': 'Hi!', 'locale:title': 'Hello!' });
+render(example, { 'locale:block': 'Block', 'locale:text': 'Hi!', 'locale:title': 'Hello!' });
