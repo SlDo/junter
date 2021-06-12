50d2fede3e10626299c0500e5975890d1f693aa2
Слава <slavadodonov19@gmail.com>
2021-06-12 03:59:07 +0300

    Features:
    1. Added linter
    2. Unused aliases will remove from DOM elements

--- scripts/index.js
+++ scripts/index.js
@@ -1,237 +1,250 @@
 const components = {};
 
-let aliasesList = {
-    slot: 'slot',
-    locale: 'locale',
-    prop: 'prop',
-    style: 'style'
+const aliasesList = {
+  slot: 'slot',
+  locale: 'locale',
+  prop: 'prop',
+  style: 'style',
+  alias: 'alias',
 };
 
-const styles = `
-    #target {
-        color: darkseagreen
-    }
-`
-
 const avatar = JSON.stringify({
-    div: {
+  div: {
+    props: {
+      class: 'logoBlock',
+    },
+    a: {
+      props: {
+        href: 'https://vk.com',
+      },
+      img: {
         props: {
-            class: 'logoBlock'
+          src: 'prop:logoSrc',
+          'tab-index': 'Hello!',
         },
-        a: {
-            props: {
-                href: "https://vk.com"
-            },
-            img: {
-                props: {
-                    src: 'prop:logoSrc'
-                }
-            },
-            style: {
-                props: {
-                    type: 'text/css'
-                },
-                content: `
-                    .logoBlock { color: red; }
-                    .logoContent { 
-                        color: red; 
-                    }
-                `
-            },
-            p: ['locale:text', 'locale:title']
-        }
-    }
-})
+      },
+      p: ['locale:text', 'locale:title'],
+      div: 'slot:avatar',
+      ul: {
+        props: {
+          class: 'names',
+        },
+        li: {
+          props: {
+            class: 'list',
+          },
+          content: [0, 'locale:string', 2, 3, 4, 5],
+        },
+      },
+    },
+  },
+});
 
 const example = JSON.stringify([
-    {
-        form: {
-           props: {
-               type: 'submit'
-           },
-           p: 'locale:welcome',
-           Avatar: {
-               slots: {
-                   'slot:block': {
-                       div: 'Hello!'
-                   }
-               }
-           }
-        }
-    }
-])
-
-const editAliasesList = (list) => {
-    aliasesList = { ...aliasesList, ...list };
-}
+  {
+    div: {
+      Avatar: {},
+      div: 'prop:alias',
+      button: 'locale:button',
+    },
+  },
+]);
 
 const getFirstLetter = (str) => str.substr(0, 1);
 const isComponent = (name) => getFirstLetter(name) === getFirstLetter(name).toUpperCase();
+const throwError = (condition, msg) => {
+  if (condition) throw new Error(msg);
+};
 
 const parseParams = (params) => {
-    if (typeof params === 'string') {
-        return {
-            props: null,
-            content: params
-        }
-    } else if (Array.isArray(params)) {
-        return {
-            props: null,
-            content: params.map((param) => parseParams(param))
-        }
-    } else if (params && params.hasOwnProperty('content')) {
-        const { props, content } = params || {};
+  const type = typeof params;
 
-        return {
-            props, content: parseParams(content).content
-        }
-    }
+  if (type === 'string' || type === 'number') {
+    return {
+      props: null,
+      content: params.toString(),
+    };
+  }
 
-    const { props, ...content } = params || {};
+  if (Array.isArray(params)) {
+    return {
+      props: null,
+      content: params.map((param) => parseParams(param)),
+    };
+  }
 
-    return { props, content };
-}
-
-const createElement = (tag, params) => {
+  if (params && Object.prototype.hasOwnProperty.call(params, 'content')) {
     const { props, content } = params || {};
 
-    const element = document.createElement(tag);
+    return {
+      props, content: parseParams(content).content,
+    };
+  }
 
-    if (content) addContent(element, content);
-    if (props) addProps(element, props);
+  const { props, ...content } = params || {};
 
-    return element;
-}
+  return { props, content };
+};
+
+const createElement = (tag, params) => {
+  const { props, content } = params || {};
+
+  const element = document.createElement(tag);
+
+  if (content) addContent(element, content);
+  if (props) addProps(element, props);
+
+  return element;
+};
 
 const addProps = (element, props) => {
-    for (const prop in props) {
-        element.setAttribute(prop.toString().toLowerCase().trim(), props[prop].toString().trim());
-    }
-}
+  for (const prop in props) {
+    throwError(props[prop] == null || typeof props[prop] === 'object', `Invalid prop value. The prop value must be a string or number. Current type: ${typeof props[prop]}. Error: ${prop}: ${props[prop]}`);
+
+    element.setAttribute(prop.toString().toLowerCase().trim(), props[prop].toString().trim());
+  }
+};
 
 const isElementTag = (element, tagName) => element.nodeName === tagName.toUpperCase();
 
 const addStyles = (element, styles) => {
-    if (isElementTag(element, 'style')) {
-        if (element.styleSheet) {
-            element.styleSheet.cssText = styles.trim();
-        } else {
-            element.appendChild(document.createTextNode(styles.trim()));
-        }
+  if (isElementTag(element, 'style')) {
+    if (element.styleSheet) {
+      element.styleSheet.cssText = styles.trim();
+    } else {
+      element.appendChild(document.createTextNode(styles.trim()));
     }
-}
+  }
+};
 
 const addContent = (element, content) => {
-    if (isElementTag(element, 'style')) {
-        addStyles(element, content);
-    } else if (typeof content === 'string') {
-        element.innerHTML = content;
-    } else if (Array.isArray(content)) {
-        content.forEach((elem) => {
-            addContent(element, elem);
-        })
-    } else {
-        if (content.hasOwnProperty('content')) {
-            return addContent(element, content.content);
-        }
-
-        addChildren(element, content);
+  if (isElementTag(element, 'style')) {
+    addStyles(element, content);
+  } else if (typeof content === 'string') {
+    element.innerHTML = content;
+  } else if (Array.isArray(content)) {
+    return content.forEach((elem) => {
+      addContent(element, elem);
+    });
+  } else {
+    if (content && Object.prototype.hasOwnProperty.call(content, 'content')) {
+      return addContent(element, content.content);
     }
-}
+
+    addChildren(element, content);
+  }
+};
+
+const aliasName = (alias) => {
+  const aliasObject = /^(.*?):/g.exec(alias);
+
+  if (aliasObject) return aliasObject[1];
+
+  return null;
+};
 
 const hasAlias = (alias) => {
-    const value = /^(.*?)\:/g.exec(alias);
+  const aliasType = aliasName(alias);
 
-    if (value) return !!aliasesList[value[1]];
-}
+  if (aliasType) return !!aliasesList[aliasType];
 
-const addAliases = (object, replacer) => {
+  return false;
+};
+
+const addAliases = (object, replacer, excludeDelete = []) => {
+  try {
     const clonedObject = JSON.parse(object);
 
     const replaceValue = (object, replacer) => {
-        for (let prop in object) {
-            const value = object[prop];
+      for (const prop in object) {
+        const value = object[prop];
 
-            if (typeof value !== "string") {
-                replaceValue(value, replacer);
-            } else {
-                if (hasAlias(value)) {
-                    const replaceValue = replacer[value];
-                    if (replaceValue) object[prop] = replaceValue;
-                }
-            }
+        if (typeof value !== 'string') {
+          replaceValue(value, replacer);
+        } else if (hasAlias(value)) {
+          const replacedValue = replacer[value];
+
+          if (!replacedValue && excludeDelete && !excludeDelete.includes(aliasName(value))) {
+            delete object[prop];
+          }
+
+          if (replacedValue) object[prop] = replacedValue;
         }
+      }
     };
 
     replaceValue(clonedObject, replacer);
 
     return clonedObject;
+  } catch (e) {
+    throwError(!!e, `Invalid JSON. Error message: ${e}`);
+  }
 };
 
 const addComponent = (element, name, props) => {
-    const component = addAliases(components[name], props);
+  const component = addAliases(components[name], props);
 
-    addContent(element, component);
-}
+  addContent(element, component);
+};
 
 const addChildren = (element, children, aliases) => {
-    for (const tag in children) {
-        if (isComponent(tag) && components[tag]) {
-            const { props, slots } = children[tag] || {};
+  for (const tag in children) {
+    if (isComponent(tag) && components[tag]) {
+      const { props, slots } = children[tag] || {};
 
-            addComponent(element, tag, { ...aliases, ...props, ...slots }, slots);
-            continue;
-        }
+      addComponent(element, tag, { ...aliases, ...props, ...slots }, slots);
+      continue;
+    }
 
-        const { content, props } = parseParams(children[tag]);
+    const { content, props } = parseParams(children[tag]);
 
-        if (Array.isArray(content)) {
-            if (content.every((elem) => typeof elem.content !== 'string')) {
-                const child = createElement(tag, { props, content });
-
-                element.appendChild(child);
-            } else {
-                content.forEach((elem) => {
-                    if (typeof elem.content === 'string') {
-                        const child = createElement(tag, { props, content: elem.content });
-
-                        element.appendChild(child);
-                    }
-                })
-            }
-
-            continue;
-        }
-
-        const child = createElement(tag, { props, content })
+    if (Array.isArray(content)) {
+      if (content.every((elem) => typeof elem.content !== 'string')) {
+        const child = createElement(tag, { props, content });
 
         element.appendChild(child);
+      } else {
+        content.forEach((elem) => {
+          if (typeof elem.content === 'string') {
+            const child = createElement(tag, { props, content: elem.content });
+
+            element.appendChild(child);
+          }
+        });
+      }
+
+      continue;
     }
-}
+
+    const child = createElement(tag, { props, content });
+
+    element.appendChild(child);
+  }
+};
 
 const render = (object, aliases) => {
-    const parsed = addAliases(object, aliases);
+  const parsed = addAliases(object, aliases);
 
-    if (Array.isArray(parsed)) {
-        return parsed.forEach((param) => {
-            for (const tag in param) {
-                //TODO: Рендеринг компонентов на верхнем уровне
-                console.log(createElement(tag, parseParams(param[tag]), aliases));
-            }
-        })
-    }
+  if (Array.isArray(parsed)) {
+    return parsed.forEach((param) => {
+      for (const tag in param) {
+        // TODO: Рендеринг компонентов на верхнем уровне
+        console.log(createElement(tag, parseParams(param[tag]), aliases));
+      }
+    });
+  }
 
-    for (const tag in parsed) {
-        //TODO: Рендеринг компонентов на верхнем уровне
-        console.log(createElement(tag, parseParams(parsed[tag]), aliases));
-    }
-}
+  for (const tag in parsed) {
+    // TODO: Рендеринг компонентов на верхнем уровне
+    console.log(createElement(tag, parseParams(parsed[tag]), aliases));
+  }
+};
 
 const registerComponent = (name, content, aliases) => {
-    if (!isComponent(name)) throw new Error('The component name must start with a capital letter');
+  throwError(!isComponent(name), 'The component name must start with a capital letter');
 
-    components[name] = JSON.stringify(addAliases(content, aliases));
-}
+  components[name] = JSON.stringify(addAliases(content, aliases, ['slot', 'prop']));
+};
 
-registerComponent('Avatar', avatar, { 'locale:text': 'Hi!', 'locale:title': 'Hello!', 'style:main': styles });
-render(example, { 'locale:block': 'Block', 'locale:text': 'Hi!', 'locale:title': 'Hello!' });
+registerComponent('Avatar', avatar, { 'locale:text': 'Hi!', 'locale:title': 'Hello!' });
+render(example, { 'prop:alias': 'Hello!' });
